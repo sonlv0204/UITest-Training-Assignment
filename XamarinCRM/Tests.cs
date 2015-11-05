f38a44a5accdb631deecbf481e87ef8846e0b48e
Sweeky <sweeky@xamarin.com>
2015-11-05 10:37:23 -0800

    Initial Commit

---
+++ XamarinCRM/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+namespace XamarinCRM
+{
+    [TestFixture]
+    public class Tests
+    {
+        AndroidApp app;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = ConfigureApp
+				.Android
+                .ApkFile ("../../../com.xamarin.xamarincrm-Signed.apk")
+				.StartApp();
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+    }
+}
+
