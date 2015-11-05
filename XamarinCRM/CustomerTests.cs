151f33cf095b6c9c3fc1c9c75058f1958079f249
Ethan Dennis <erdennis13@gmail.com>
2015-11-05 11:10:17 -0800

    Added check customer details test

--- XamarinCRM/CustomerTests.cs
+++ XamarinCRM/CustomerTests.cs
@@ -13,7 +13,19 @@ namespace XamarinCRM
         [Test]
         public void CheckCustomerDetails()
         {
-            app.Repl();
+            app.Tap(x => x.Class("FormsTextView").Text("SKIP SIGN IN (demo)"));
+            app.Screenshot("Tapped on view FormsTextView with Text: 'SKIP SIGN IN (demo)'");
+
+            app.Tap(x => x.Class("TextView").Text("Customers"));
+            app.Screenshot("Tapped on view TextView with Text: 'Customers'");
+
+            app.Tap((x => x.Class("ViewCellRenderer_ViewCellContainer").Index(0)));
+            app.Screenshot("Selected first customer present");
+
+            app.WaitForElement(x => x.Class("FormsTextView").Text("Contact"));
+            app.WaitForElement(x => x.Class("FormsTextView").Text("Phone"));
+            app.WaitForElement(x => x.Class("FormsTextView").Text("Address"));
+            app.Screenshot("Contact info verified as present");
         }
     }
 }
