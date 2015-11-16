03a052fe9f28ad7243e147a6c29e401d8266fc88
Charles Wang <charles.wang@xamarin.com>
2015-11-16 11:20:19 -0800

    Added assignments

---
+++ Best.Practices/AppInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+namespace Best.Practices
+{
+    [TestFixture]
+    public abstract class BaseTest
+    {
+        protected AndroidApp app;
+
+        protected BaseTest()
+        {
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = ConfigureApp
+				.Android
+                .ApkFile ("../../../com.xamarin.xamarincrm-Signed.apk")
+				.StartApp();
+        }
+
+    }
+}
+
---
+++ Best.Practices/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Best.Practices
+{
+    public class Test :BaseTest
+    {
+        public Test()
+        {
+        }
+
+        [Test]
+        public void TryCatchTest()
+        {
+        }
+
+        [Test]
+        public void LoopTest()
+        {
+        }
+    }
+}
+
